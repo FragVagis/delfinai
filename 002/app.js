72ccd1b1cf1f31b1bf5e9d86359559736c7b7e5b
dmitrij <dzimaskurb@gmail.com>
2022-07-14 17:01:54 +0300

    002-forEach-map-sort-filter

---
+++ 002/app.js
@@ -0,0 +1,63 @@
+ //forEach; map; sort; filter
+ console.log('---forEach--start-----');
+[1, 5, 77].forEach(a => console.log(a));
+
+console.log('-----<>-----');
+//same shit tik <> keiciasi -------
+[1, 5, 77].forEach(a => {
+    if(a < 4) {
+        console.log(a)
+    }
+});
+
+console.log('---same---');
+
+[1, 5, 77].forEach(a => {
+    if(a > 4) {
+        console.log(a)
+      }
+});
+//same shit ------------------------
+console.log('-----<>-----');
+//  (i) - tai yra indeksas (Numeravimas nuo nulio (0))
+[1, 5, 77].forEach((a, i) => console.log(a, i));
+console.log('---forEach--end-----');
+
+
+
+console.log('---map--start-----');
+const fe = [1, 5, 77].forEach(a => console.log(a));
+const map = [1, 5, 77].map((a, i) => {
+
+    return a * i
+
+});
+console.log(fe);
+console.log(map);
+console.log('---map--end-----');
+
+
+console.log('---filter--start-----');
+const fi = [1, 0, 5, 77].filter(a => a > 4).map(r => r * 10);
+
+console.log(fi);
+console.log('---filter--end-----');
+
+
+
+console.log('---sort--start-----');
+
+const namas1 = [1, 10, 660, 0, 5, 77];
+const namas2 = [1, 10, 660, 0, 5, 77];
+namas1.sort((a, b) => b - a);
+namas2.sort((a, b) => a - b);
+
+console.log(namas1);
+console.log(namas2);
+
+
+console.log('---sort--end-----');
+
+
+
+
