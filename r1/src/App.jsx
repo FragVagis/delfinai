57bad0cdca750eef5102942587fead9b9538b07c
dmitrij <dzimaskurb@gmail.com>
2022-07-20 13:54:57 +0300

    React

---
+++ r1/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Kosmosas from './Components/005/Kosmosas';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Zdarova maladoi!</h1>
+        <Kosmosas></Kosmosas>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ r1/src/Components/005/Kosmosas.jsx
@@ -0,0 +1,20 @@
+import rand from '../../Functions/rand';
+function Kosmosas() {
+
+    return (
+
+    <>
+    <div style={
+        {
+            color: 'crimson',
+            fontSize: rand(30, 100) + 'px',
+            padding: '10px'
+        }
+        }>MAR-SAS</div>
+    <div>KOS-MOS</div>
+    </>
+    );
+
+}
+
+export default Kosmosas;
