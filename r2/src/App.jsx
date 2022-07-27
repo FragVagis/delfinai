dc6e59842a2c5b4bc4d2a6cdba6d22aca8fa737c
dmitrij <dzimaskurb@gmail.com>
2022-07-27 14:33:05 +0300

    R2

---
+++ r2/src/App.jsx
@@ -0,0 +1,34 @@
+import './App.scss';
+import './bootstrap.css';
+import AnimalsContext from './Components/AnimalsContext';
+import Create from './Components/Create';
+
+const animalsTypes = [
+    {id: 1, type: 'Antis'},
+    {id: 2, type: 'Avis'},
+    {id: 3, type: 'Antilopas'},
+    {id: 4, type: 'Bebras'},
+    {id: 5, type: 'Briedis'},
+    {id: 6, type: 'Barsukas'}
+];
+
+function App() {
+    return (
+        <AnimalsContext.Provider value={{
+            animalsTypes
+        }}>
+        <div className="container">
+            <div className="row">
+                <div className="col-4">
+                    <Create/>
+                </div>
+                <div className="col-8">
+                    One of three columns
+                </div>
+            </div>
+        </div>
+        </AnimalsContext.Provider>
+    );
+}
+
+export default App;
---
+++ r2/src/Components/Create.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { useContext } from "react";
+import AnimalsContext from "./AnimalsContext";
+
+function Create() {
+
+    const { animalsTypes } = useContext(AnimalsContext);
+    const [type, setType] = useState(1);
+    const [weight, setWeight] = useState('');
+
+    return (
+        <div className="card m-4">
+            <h5 className="card-header">Add new Animal</h5>
+            <div className="card-body">
+                <div className="form-group">
+                    <label for="exampleFormControlSelect2">Example multiple select</label>
+                    <select className="form-control" value={type} onChange={e => setType(e.target.value)}>
+                        {
+                            animalsTypes.map(a => <option key={a.id} value={a.id}>{a.type}</option>)
+                        }
+                    </select>
+                </div>
+                <div className="form-group">
+                    <label>How much is the {animalsTypes.filter(a => a.id == type).type}?</label>
+                    <input type="text" className="form-control" value={weight} onChange={e => setWeight(e.target.value)}/>
+                        <small className="form-text text-muted">Please, enter your animal weight in kg here.</small>
+                </div>
+                <button type="button" className="btn btn-outline-info m-2">Add this creature</button>
+            </div>
+        </div>
+    );
+
+}
+
+export default Create;
